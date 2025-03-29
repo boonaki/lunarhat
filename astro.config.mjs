ced721e1a717ac40fa1b4ece61fca922ce890fcb
boonaki <joshdurr17@gmail.com>
2025-03-29 00:15:28 -0400

    added netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 
 import svelte from '@astrojs/svelte';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: netlify()
 });
